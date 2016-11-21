fbb40f6ade0ec4107f6064d1559b1419bfa3b463
Finley Siebert <finleysiebert@outlook.com>
2016-11-21 23:13:46 +0100

    Fixed roles updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-  validates :email, :presence => true, :uniqueness => true
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }, :format => { :with => /\A(?=.*[a-z])[a-z\d]+\Z/i }
+  validates :email, :presence => true, :uniqueness => true, :format => { :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 6..20, :on => :create
 
@@ -11,14 +11,13 @@ class User < ActiveRecord::Base
   has_many :topics, :foreign_key => :author_id
 
   def has_role(id)
-    role = Role.find(id)
+    roles = Rolesuser.where(:user_id => self.id, :role_id => id).first
 
-    if self.primary_role == role.id
-      true
-    else
+    if roles.nil?
       false
+    else
+      true
     end
-
   end
 
 
